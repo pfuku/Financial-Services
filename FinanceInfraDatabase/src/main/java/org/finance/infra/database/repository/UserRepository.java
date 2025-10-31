95eadeabde6f957f819dbe1e00974616e72ba412
Ufuk Tiryaki <ufuk.tiryaki@gmail.com>
2025-10-31 15:15:56 +0300

    Added Finder

--- FinanceInfraDatabase/src/main/java/org/finance/infra/database/repository/UserRepository.java
+++ FinanceInfraDatabase/src/main/java/org/finance/infra/database/repository/UserRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     UserEntity findByUserNameAndState(String userName, Integer state);
     UserEntity findByIdAndStatus(Long Id, Integer status );
+    UserEntity findByIdAndStatusAndState(Long Id, Integer status, Integer state);
 }
